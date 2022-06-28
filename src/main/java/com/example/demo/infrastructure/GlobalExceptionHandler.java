da96ca438bda279a2229649134a562730a6d992f
f-lab-fi <93929454+f-lab-fi@users.noreply.github.com>
2022-06-28 23:51:09 +0900

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/example/demo/infrastructure/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.demo.presentation;
+
+import com.example.demo.domain.NewUrlNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NewUrlNotFoundException.class)
+    public ResponseEntity handleNotFoundException(NewUrlNotFoundException e) {
+        return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleAllException(Exception e) {
+        return new ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+};
