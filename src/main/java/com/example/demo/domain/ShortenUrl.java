d52d07a9f241f076487c9826fa0d3a015d9c8d85
reneener <jhjin2u@gmail.com>
2022-07-19 21:59:41 +0900

    2차

--- src/main/java/com/example/demo/domain/ShortenUrl.java
+++ src/main/java/com/example/demo/domain/ShortenUrl.java
@@ -3,7 +3,6 @@ package com.example.demo.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 import java.util.UUID;
 
 @Getter
@@ -11,15 +10,15 @@ import java.util.UUID;
 @ToString
 public class ShortenUrl {
     private String id;
-    private String prevUrl;
+    private String destination;
     private String newUrl;
     private int cnt;
 
-    public ShortenUrl(String prevUrl, String newUrl ){
+    public ShortenUrl(String destination, String newUrl){
         this.id = UUID.randomUUID().toString().replace("-", "");
-        this.prevUrl = prevUrl;
+        this.destination = destination;
         this.newUrl = newUrl;
-        this.cnt = 1;
+        this.cnt = 0;
     }
 
     public void countUp() {
