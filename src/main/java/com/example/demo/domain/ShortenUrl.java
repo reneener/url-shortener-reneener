9e6806c29e3320edc23b3c29787d5602cd20a1f7
reneener <jhjin2u@gmail.com>
2022-12-09 17:46:00 +0900

    mysql 연동

--- src/main/java/com/example/demo/domain/ShortenUrl.java
+++ src/main/java/com/example/demo/domain/ShortenUrl.java
@@ -1,26 +1,42 @@
 package com.example.demo.domain;
 
+import com.example.demo.domain.user.User;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import java.util.UUID;
+import org.hibernate.validator.constraints.URL;
 
 @Getter
 @Setter
 @ToString
+@Entity
 public class ShortenUrl {
-    private String id;
+
+    @Id @GeneratedValue
+    private long id;
+    @URL
     private String destination;
+    @URL
     private String newUrl;
+    @ManyToOne
+    @JoinColumn(name="USER_ID")
+    private User user;
+
     private int cnt;
 
     @Builder
-    public ShortenUrl(String destination, String newUrl){
-        this.id = UUID.randomUUID().toString().replace("-", "");
+    public ShortenUrl(String destination, String newUrl, String userId){
         this.destination = destination;
         this.newUrl = newUrl;
         this.cnt = 0;
+        this. = userId;
     }
 
     public void countUp() {
