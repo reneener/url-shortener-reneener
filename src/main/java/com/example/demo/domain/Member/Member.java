ef4ad605db13cc10e47f8f50f0bc24590179a837
reneener <jhjin2u@gmail.com>
2022-12-09 17:59:21 +0900

    ShortenUrl domain 수정

--- src/main/java/com/example/demo/domain/Member/Member.java
+++ src/main/java/com/example/demo/domain/Member/Member.java
@@ -8,8 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter @Setter
 public class Member {
 
 	@Id
--- src/main/java/com/example/demo/domain/ShortenUrl.java
+++ src/main/java/com/example/demo/domain/ShortenUrl.java
@@ -1,7 +1,8 @@
 package com.example.demo.domain;
 
-import com.example.demo.domain.user.User;
+import com.example.demo.domain.Member.Member;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -10,7 +11,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import java.util.UUID;
 import org.hibernate.validator.constraints.URL;
 
 @Getter
@@ -21,22 +21,32 @@ public class ShortenUrl {
 
     @Id @GeneratedValue
     private long id;
+
     @URL
     private String destination;
+
     @URL
     private String newUrl;
-    @ManyToOne
-    @JoinColumn(name="USER_ID")
-    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="MEMBER_ID")
+    private Member member;
 
     private int cnt;
 
     @Builder
-    public ShortenUrl(String destination, String newUrl, String userId){
+    public ShortenUrl(String destination, String newUrl){
         this.destination = destination;
         this.newUrl = newUrl;
         this.cnt = 0;
-        this. = userId;
+    }
+
+    public ShortenUrl() {
+    }
+
+    public void setMember(Member member){
+        this.member = member;
+        member.getUrls().add(this);
     }
 
     public void countUp() {
