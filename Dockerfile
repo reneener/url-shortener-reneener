dcefc7dfcb7c1856981f0b0bb7b744c3ca87e9a5
harper <jhjin2u@gmail.com>
2023-03-14 05:15:38 +0900

    feat: Create Shortener URL with front testing

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:11
-ARG JAR_FILE=build/libs/app.jar
-COPY ${JAR_FILE} ./app.jar
-ENV TZ = Asiz/Seoul
-ENTRYPOINT ["java", "-jar", "./app.jar"]
---
+++ src/main/java/com/example/demo/presentation/dto/UrlDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UrlRequest {
+	private String destination;
+}
